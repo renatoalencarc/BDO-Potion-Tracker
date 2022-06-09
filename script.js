0981d8f33cf1abf6b753519a8c5d6e311e1cb104
Renato Alencar <renatoalencar.dev@outlook.com>
2022-06-09 12:17:53 -0300

    First Commit

---
+++ script.js
@@ -0,0 +1,15 @@
+// Nav Script
+function navFunction() {
+    var x = document.getElementById("navigation");
+    if (x.className === "myNav") {
+        x.className += "responsive";
+    } else {
+        x.className = "myNav";
+    }
+}
+
+function inputFunction() {
+    document.open()
+    document.write()
+    document.close()
+}
